1a10f903ed584f3783bd411deb109fecd6bdb633
Shahid Sayyad <ssayyad@AffinionDS.Com>
2020-05-26 20:50:47 +0530

    added complete hotel search test

--- src/test/java/HotelSearchTest.java
+++ src/test/java/HotelSearchTest.java
@@ -2,6 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -9,18 +12,20 @@ import java.util.concurrent.TimeUnit;
 public class HotelSearchTest {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     @Test
     public void HotelSearchForLASLocationTest(){
         String currentUserDirectory = System.getProperty("user.dir");
         System.setProperty("webdriver.chrome.driver", currentUserDirectory + "\\src\\test\\resources\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("http://cltsenvironments.loyaltytravelservices.com/SSOSimulator/");
 
-        WebElement productDropDown = driver.findElement(By.xpath("(//*[@class=\\'btn dropdown-toggle selectpicker btn-default\\'])[2]"));
-        WebElement templarSelectionDropDown = driver.findElement(By.xpath("(//*[@class='btn dropdown-toggle selectpicker btn-default'])[3]"));
+        //WebElement productDropDown = driver.findElement(By.xpath("(//*[@class='btn dropdown-toggle selectpicker btn-default'])[2]"));
+        //WebElement templarSelectionDropDown = driver.findElement(By.xpath("(//*[@class='btn dropdown-toggle selectpicker btn-default'])[3]"));
 
         WebElement environmentDropDown = driver.findElement(By.xpath("(//*[@class='btn dropdown-toggle selectpicker btn-default'])[4]"));
         environmentDropDown.click();
@@ -51,5 +56,34 @@ public class HotelSearchTest {
         pointBalanceTextBox.sendKeys("600000");
         WebElement goButton = driver.findElement(By.xpath("//*[@class='btn btn-primary']"));
         goButton.click();
+
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#search-hotel-button")));
+        WebElement hotelSearchNavButton = driver.findElement(By.cssSelector("#search-hotel-button"));
+        hotelSearchNavButton.click();
+
+        WebElement destinationTextBox = driver.findElement(By.cssSelector("#inputDestination"));
+        destinationTextBox.sendKeys("LAS");
+        WebElement destinationLocation = driver.findElement(By.xpath("(//*[@class='autosuggest-item ui-menu-item'])['1']"));
+        destinationLocation.click();
+
+        WebElement checkInDateInputBox = driver.findElement(By.cssSelector("#inputCheckInDate"));
+        checkInDateInputBox.click();
+        WebElement checkInDate = driver.findElement(By.xpath("//a[contains(@id,'28')]"));
+        checkInDate.click();
+
+        WebElement checkOutDateInputBox = driver.findElement(By.cssSelector("#inputCheckOutDate"));
+        checkOutDateInputBox.click();
+        WebElement checkOutDate = driver.findElement(By.xpath("//a[contains(@id,'29')]"));
+        checkOutDate.click();
+
+        WebElement hotelSearchButton = driver.findElement(By.cssSelector("#hotelSearchButton"));
+        hotelSearchButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//*[contains(@id,'ChooseRoom')])['1']")));
+        WebElement chooseRoomButton = driver.findElement(By.xpath("(//*[contains(@id,'ChooseRoom')])['1']"));
+        Assert.assertTrue(chooseRoomButton.isDisplayed());
+
+        driver.close();
     }
 }
