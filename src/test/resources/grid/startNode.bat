618ff60bf14bb30a64305d36593f697bc7b7f52b
Shahid Sayyad <ssayyad@AffinionDS.Com>
2020-05-30 20:14:23 +0530

    added support to run testNG xml from pom.xml

---
+++ src/test/resources/grid/startNode.bat
@@ -0,0 +1,1 @@
+java -Dwebdriver.chrome.driver="C:\02 personal\Learning\Shahid\Frameworks\Framework02\src\test\resources\drivers\chromedriver.exe" -jar -Dwebdriver.gecko.driver="C:\02 personal\Learning\Shahid\Frameworks\Framework02\src\test\resources\drivers\geckodriver.exe"  selenium-server-standalone-3.141.59.jar -role node -nodeConfig nodeConfig.json
